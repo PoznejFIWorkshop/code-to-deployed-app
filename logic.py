76038c006d41a71fbd48325a961de1476737666c
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2023-02-17 17:42:13 +0100

    Add factorial function and CLI input

---
+++ logic.py
@@ -0,0 +1,20 @@
+# This code is intentionaly badly written - you should fix it. :)
+# You can get help at https://edulint.com/
+
+
+def factorial(n: int) -> int:
+    assert n >= 0, "This factorial function only works for n >= 0"
+
+    if n in [0, 1]:
+        return 1
+    else:
+        answer = 1
+        for i in range(2, n+1):
+            answer = i * answer
+        return answer
+
+
+if __name__ == "__main__":
+    n = input("Input n for calculation of factorial: ")
+    answer = factorial(int(n))
+    print(f"{n}! = {answer}")
